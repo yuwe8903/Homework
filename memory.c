676fce60b60d222a1a5e89cfd4ec95f046fad2d8
Yue Wei <yuwe8903@colorado.edu>
2018-01-31 14:21:34 -0700

    fixed memory.c

--- memory.c
+++ memory.c
@@ -1,6 +1,6 @@
 #include "memory.h"
 
-uint8_t * my_memzero(uint8_t * src, size_t length);{
+uint8_t * my_memzero(uint8_t * src, size_t length){
   for(uint8_t i = 0, i < length, i++){
     *src = 0;
     src++;
